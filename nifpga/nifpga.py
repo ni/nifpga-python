44cf9b93987f82637cfa8cbdac1ba6914231b7c2
Mose Gumble <mosegumble@gmail.com>
2017-03-14 12:51:52 -0500

    Fix Style
    Removed redundant backslash

--- nifpga/nifpga.py
+++ nifpga/nifpga.py
@@ -236,7 +236,7 @@ class _NiFpga(StatusCheckedLibrary):
             LibraryFunctionInfo(
                 pretty_name="ClientFunctionCall",
                 name_in_library="NiFpgaDll_ClientFunctionCall",
-                named_argtypes=\
+                named_argtypes=
                 [
                     NamedArgtype("session", _SessionType),
                     NamedArgtype("group", ctypes.c_uint32),
