981df1738ed142089b3b033a1aab432e19cb516f
Mose Gumble <mosegumble@gmail.com>
2018-02-15 10:59:18 -0600

    Adding more unit tests

--- nifpga/tests/test_FXP.py
+++ nifpga/tests/test_FXP.py
@@ -88,7 +88,6 @@ class FxpRegisterConvertFromBinaryToDecimalValues(unittest.TestCase):
         self.assertEqual(expected, actual)
         self.assertTrue(FxpRegister.overflow)
 
-
     def test_16bit_all_fractional(self):
         FxpRegister = MockFxpRegister(signed=False,
                                       enableOverflowStatus=False,
@@ -120,17 +119,48 @@ class FxpRegisterConvertFromBinaryToDecimalValues(unittest.TestCase):
                                       word_length=15,
                                       integer_word_length=0)
         actual = FxpRegister._convert_from_binary_to_decimal(binary_string_16bit)
-        expected = Decimal(0)
+        """ The expected value should be equal to
+        2^(-3) + 2^(-4) + 2^(-6) + 2^(-7) + 2^(-9) + 2^(-10) + 2^(-12) + 2^(-14)
+        """
+        expected = Decimal(-0.21417236328125)
+        self.assertEqual(expected, actual)
+        self.assertTrue(FxpRegister.overflow)
+
+    def test_16bit_integer_16bit_fractional(self):
+        FxpRegister = MockFxpRegister(signed=False,
+                                      enableOverflowStatus=False,
+                                      word_length=32,
+                                      integer_word_length=16)
+        #'0100010110010010.0011000100001100'
+        """ The expected value should be equal to
+        2^(1) + 2^(4) + 2^(7) + 2^(8) + 2^(10) + 2^(14) +
+        2^(-3) + 2^(-4) + 2^(-8) + 2^(-13) + 2^(-14)
+        """
+        actual = FxpRegister._convert_from_binary_to_decimal(binary_string_32bit)
+        expected = Decimal(17810.19158935546875)
         self.assertEqual(expected, actual)
 
-    # def test_16bit_integer_16bit_fractional(self):
-    #     FxpRegister = MockFxpRegister(signed=True,
-    #                                   enableOverflowStatus=True,
-    #                                   word_length=32,
-    #                                   integer_word_length=16)
-    #     actual = FxpRegister._convert_from_binary_to_decimal(binary_string_16bit)
-    #     expected = Decimal(0)
-    #     self.assertEqual(expected, actual)
+    def test_16bit_integer_15bit_fractional_overflow_signed(self):
+        FxpRegister = MockFxpRegister(signed=True,
+                                      enableOverflowStatus=True,
+                                      word_length=31,
+                                      integer_word_length=16)
+        """
+        #'01000101100100100.011000100001100'
+        Expect there to not be an overflow, the value to be negative and the
+        twos compliment of the 31 bits of data is:
+        0111010011011011.10011101111010
+        The expected value should be equal to
+        2^(0) + 2^(1) + 2^(3) + 2^(4) + 2^(6) + 2^(7) + 2^(10) + 2^(12) +
+        2^(13) + 2^(14) +
+        2^(-1) + 2^(-4) + 2^(-5) + 2^(-6) + 2^(-8) + 2^(-9) + 2^(-10) +
+        2^(-11)+ 2^(-13)
+        """
+        actual = FxpRegister._convert_from_binary_to_decimal(binary_string_32bit)
+        expected = Decimal(-29915.6168212890625)
+        self.assertEqual(expected, actual)
+        self.assertFalse(FxpRegister.overflow)
+
 
     # def test_500bit_integer_32bit_word(self):
     #     FxpRegister = MockFxpRegister(signed=False,
@@ -138,8 +168,10 @@ class FxpRegisterConvertFromBinaryToDecimalValues(unittest.TestCase):
     #                                   word_length=32,
     #                                   integer_word_length=468)
     #     actual = FxpRegister._convert_from_binary_to_decimal(binary_string_32bit)
-    #     expected = Decimal(0)
-    #     self.assertEqual(expected, actual)
+    #     # With really large numbers, python can still equate equality using
+    #     # E-Notation with enough significant digits
+    #     expected = Decimal('2.07122190948279619911823418E140')
+    #     self.assertTrue(expected == actual, "Expected {} Actual {}".format(expected, actual))
 
     # def test_500bit_fractional_32bit_word(self):
     #     FxpRegister = MockFxpRegister(signed=False,
